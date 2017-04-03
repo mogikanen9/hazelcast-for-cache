f58750ba47583b3a07718e458f29a9074bee3224
Vladislav V <Vladislav V>
2017-04-02 21:21:45 -0400

    Simple Hz integration
     * hazelcast.jar and hazelcast.xml in classpath
     * spring-boot automatically creates one Hz instance
     * we can use for anything, ex. cache results of listing employees

--- spring-boot-hazelcast-sample/src/main/java/com/mogikanensoftware/cache/service/impl/EmployeeServiceImpl.java
+++ spring-boot-hazelcast-sample/src/main/java/com/mogikanensoftware/cache/service/impl/EmployeeServiceImpl.java
@@ -1,23 +1,39 @@
 package com.mogikanensoftware.cache.service.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.hazelcast.core.HazelcastInstance;
 import com.mogikanensoftware.cache.bean.Employee;
 import com.mogikanensoftware.cache.service.EmployeeService;
 
 @Component
 public class EmployeeServiceImpl implements EmployeeService {
 
+	private HazelcastInstance hazelcastInstance;
+	
+	@Autowired
+	public EmployeeServiceImpl(HazelcastInstance hazelcastInstance) {
+		super();
+		this.hazelcastInstance = hazelcastInstance;
+	}
+
 	@Override
 	public List<Employee> listAll() {
-		List<Employee> result = new ArrayList<>();
-		for(int i=0;i<10;i++){
-			result.add(new Employee("Jenny "+i,"Doe"+i,1500+i));
+		
+		List<Employee> employees = this.hazelcastInstance.getList("emloyees");
+		if(employees.isEmpty()){
+			System.out.println("Creating empoyees...");
+			for(int i=0;i<10;i++){
+				employees.add(new Employee("Jenny "+i,"Doe"+i,1500+i));
+			}
+		}else{
+			System.out.println("Getting employees from cache");
 		}
-		return result;
+		
+		return employees;
 	}
 
 }
