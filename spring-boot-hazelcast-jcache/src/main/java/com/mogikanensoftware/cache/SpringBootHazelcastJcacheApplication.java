54ac41bae3b520eb5343b504f627f1e9ecebc793
Vladislav V <Vladislav V>
2017-04-04 23:49:53 -0400

    Runnable but not workable
    JCache configuration issues

--- spring-boot-hazelcast-jcache/src/main/java/com/mogikanensoftware/cache/SpringBootHazelcastJcacheApplication.java
+++ spring-boot-hazelcast-jcache/src/main/java/com/mogikanensoftware/cache/SpringBootHazelcastJcacheApplication.java
@@ -1,9 +1,17 @@
 package com.mogikanensoftware.cache;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.hazelcast.HazelcastAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+        // disable Hazelcast Auto Configuration, and use JCache configuration for the member example
+        HazelcastAutoConfiguration.class
+})
+@EnableCaching
 public class SpringBootHazelcastJcacheApplication {
 
 	public static void main(String[] args) {
--- spring-boot-hazelcast-jcache/src/main/java/com/mogikanensoftware/cache/service/EventService.java
+++ spring-boot-hazelcast-jcache/src/main/java/com/mogikanensoftware/cache/service/EventService.java
@@ -3,11 +3,15 @@ package com.mogikanensoftware.cache.service;
 import java.util.Collection;
 import java.util.Optional;
 
+import javax.cache.annotation.CacheResult;
+
 import com.mogikanensoftware.cache.bean.Event;
 
 public interface EventService {
 
+	@CacheResult(cacheName="events")
 	Optional<Event> getById(String eventId);
 
+	@CacheResult(cacheName="allEvents")
 	Collection<Event> getAll();
 }
